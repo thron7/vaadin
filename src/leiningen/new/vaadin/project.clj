31e3a2902ac538d128863cf814068d30d0c47844
thron7 <thron7@users.sourceforge.net>
2018-10-14 11:46:04 +0200

    missing dependency and added scope

--- src/leiningen/new/vaadin/project.clj
+++ src/leiningen/new/vaadin/project.clj
@@ -7,8 +7,9 @@
                       [com.vaadin.external.flute/flute]
                       [com.vaadin.external.google/guava]]]
                  [com.vaadin/vaadin-client-compiled "7.3.1"]
+                 [com.vaadin/vaadin-client "7.3.1" :scope "provided"]
                  [com.vaadin/vaadin-themes "7.3.1"]
-                 [javax.servlet/javax.servlet-api "4.0.1"]]
+                 [javax.servlet/javax.servlet-api "4.0.1" :scope "provided"]]
   :aot [{{name}}.{{name}}ui]
   :plugins [[lein-servlet "0.4.1"]]
   
