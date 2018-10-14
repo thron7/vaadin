5ab05753991129d34f5d2c78591c5b07e32b9c4b
thron7 <thron7@users.sourceforge.net>
2018-10-14 10:43:57 +0200

    updated dependencies; exclusions from vaadin/hello-world

--- src/leiningen/new/vaadin/project.clj
+++ src/leiningen/new/vaadin/project.clj
@@ -2,15 +2,17 @@
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
 
-  :dependencies [[org.clojure/clojure "1.5.1"]
-		 [com.vaadin/vaadin-server "7.0.3"]
-                 [com.vaadin/vaadin-client-compiled "7.0.3"]
-                 [com.vaadin/vaadin-themes "7.0.3"]
-                 [javax.servlet/servlet-api "2.5"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+            		 [com.vaadin/vaadin-server "7.3.1" :exclusions [
+                      [com.vaadin.external.flute/flute]
+                      [com.vaadin.external.google/guava]]]
+                 [com.vaadin/vaadin-client-compiled "7.3.1"]
+                 [com.vaadin/vaadin-themes "7.3.1"]
+                 [javax.servlet/javax.servlet-api "4.0.1"]]
   :aot [{{name}}.{{name}}ui]
-  :plugins [[lein-servlet "0.3.0"]]
+  :plugins [[lein-servlet "0.4.1"]]
   
-  :servlet {:deps [[lein-servlet/adapter-jetty7 "0.3.0"]]
+  :servlet {:deps [[lein-servlet/adapter-jetty7 "0.4.1"]]
             :config {:engine :jetty
                      :host "localhost"
                      :port 3000}
